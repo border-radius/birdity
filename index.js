8f375579dead3a8faf00ea14048747285fcdef05
border-radius <pharshmaster@gmail.com>
2014-11-19 14:57:45 +0300

    Start UI library prototyping

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var less = require('less');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('static'));
+
+app.get('/birdity.css', function (req, res) {
+	fs.readFile('./birdity.less', { encoding: 'utf8' }, function (e, css) {
+		if (e) return res.status(500).send(e);
+		
+		less.render(css, function (e, css) {
+			if (e) return res.status(500).send(e);
+			res.set('Content-type', 'text/css').send(css);
+		});
+	});
+});
 
 app.post('/mail', function (req, res) {
 	fs.appendFileSync('mails', req.param('mail') + '\n');
