7240d739d057603dc5aa5be022777b24757f50a0
border-radius <pharshmaster@gmail.com>
2014-12-02 14:33:26 +0300

    .write form fixes

--- birdity.js
+++ birdity.js
@@ -20,8 +20,26 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 	$locationProvider.html5Mode(true);
 }]);
 
-app.directive('autoresize', ['$timeout', function ($timeout) {
+app.directive('magic', ['$timeout', function ($timeout) {
 	return function (scope, elem, attrs) {
+
+		//send
+		elem.on('keydown', function (event) {
+			if (event.keyCode == 13) {
+				$timeout(function () {
+					elem.val('');
+				});
+			}
+		});
+
+		//prevent multilines
+		elem.on('paste', function () {
+			$timeout(function () {
+				elem.val(elem.val().replace(/\n/g, ' '));
+			});
+		});
+
+		//autoresize
 		var line;
 		elem.on('change cut paste drop keydown', function () {
 			if (!line) line = elem[0].style.height;
