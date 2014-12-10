d79af857a9f3363a29d2ed4ef937b9211ab0db8a
border-radius <pharshmaster@gmail.com>
2014-12-10 14:06:52 +0300

    User model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,30 @@
 
 module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      isNull: false,
+      unique: true,
+      is: {
+        args: ["^([A-z0-9 -]+|[А-я0-9 -]+)$", "i"],
+        msg: 'Только латинские, либо только кириллические символы. Еще можно цифры, минус и пробел.'
+      },
+      len: {
+        args: [2, 24],
+        msg: 'От 2 до 24 символов'
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      isNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      isEmail: {
+        msg: 'Настоящий e-mail, пожалуйста.'
+      },
+      unique: true
+    },
     inviteCode: DataTypes.STRING
   }, {
     classMethods: {
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
